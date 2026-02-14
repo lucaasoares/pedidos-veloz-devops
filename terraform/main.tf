19de192fa8652fcd73f8a93cd3e239a8d35fd47d
Lucas Ribeiro <contatolucasribeirosoares@gmail.com>
2026-02-13 23:27:05 -0300

    Atualização esqueleto terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,8 @@
 terraform {
   required_providers {
     kubernetes = {
-      source = "hashicorp/kubernetes"
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
     }
   }
 }
@@ -10,4 +11,36 @@ provider "kubernetes" {
   config_path = "~/.kube/config"
 }
 
+# Nome do projeto
+resource "kubernetes_namespace" "pedidos_veloz" {
+  metadata {
+    name = "pedidos-veloz"
+  }
+}
 
+# Simula as configs da aplicação
+resource "kubernetes_config_map" "app_config" {
+  metadata {
+    name      = "app-config"
+    namespace = kubernetes_namespace.pedidos_veloz.metadata[0].name
+  }
+
+  data = {
+    APP_ENV = "production"
+    LOG_LEVEL = "info"
+  }
+}
+
+# Simula a segurança das credencias do banco
+resource "kubernetes_secret" "db_secret" {
+  metadata {
+    name      = "db-secret"
+    namespace = kubernetes_namespace.pedidos_veloz.metadata[0].name
+  }
+
+  data = {
+    POSTGRES_PASSWORD = base64encode("postgres")
+  }
+
+  type = "Opaque"
+}
